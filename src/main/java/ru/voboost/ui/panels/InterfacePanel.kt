f840eb88b3b44eb8d263cf7336b2cfc0b0eeecd2
Vitaly Harisov <vitaly@harisov.name>
2026-02-27 23:20:39 +0300

    refactor(panels): rebuild application panels using exclusive library components

--- src/main/java/ru/voboost/ui/panels/InterfacePanel.kt
+++ src/main/java/ru/voboost/ui/panels/InterfacePanel.kt
@@ -1,16 +1,16 @@
 package ru.voboost.ui.panels
 
 import ru.voboost.config.models.Tab
-import ru.voboost.ui.ConfigViewModel
-import ru.voboost.ui.components.Panel
+import ru.voboost.ui.ConfigState
+import ru.voboost.ui.components.PanelDefinition
 import ru.voboost.ui.components.panel
 
 fun createInterfacePanel(
-    @Suppress("UNUSED_PARAMETER") configViewModel: ConfigViewModel,
-): Panel {
+    @Suppress("UNUSED_PARAMETER") configState: ConfigState,
+): PanelDefinition {
     return panel(Tab.`interface`, "tab_interface") {
-        section("placeholder", "tab_interface") {
-            info(
+        section("interface_placeholder", "tab_interface") {
+            text(
                 id = "interface_placeholder",
                 textKey = "interface_placeholder_text",
             )
